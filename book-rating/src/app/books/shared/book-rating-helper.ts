d45d8ed613920e87915e7309dc89d4c4cb4e814b
Ferdinand Malcher <ferdinand@malcher.media>
2026-09-09 15:00:26 +0200

    Grundgerüst BookRatingHelper

---
+++ book-rating/src/app/books/shared/book-rating-helper.ts
@@ -0,0 +1,13 @@
+import { Service } from '@angular/core';
+import { Book } from './book';
+
+@Service()
+export class BookRatingHelper {
+    rateUp(book: Book): Book {
+        return book; // TODO
+    }
+
+    rateDown(book: Book): Book {
+        return book; // TODO
+    }
+}

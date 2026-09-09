2be729a0e5e2a568de402dd7289e6294b9986066
Ferdinand Malcher <ferdinand@malcher.media>
2026-09-09 11:27:30 +0200

    Signal mit Buchliste

--- book-rating/src/app/books/dashboard-page/dashboard-page.ts
+++ book-rating/src/app/books/dashboard-page/dashboard-page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Book } from '../shared/book';
 
 @Component({
   imports: [],
@@ -6,4 +7,29 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard-page.scss',
   templateUrl: './dashboard-page.html',
 })
-export class DashboardPage {}
+export class DashboardPage {
+  protected readonly books = signal<Book[]>([]);
+
+  constructor() {
+    this.books.set([
+      {
+        isbn: '123',
+        title: 'Angular',
+        description: 'Das große Praxisbuch',
+        authors: ['Ferdinand Malcher', 'Danny Koppenhagen', 'Johannes Hoppe'],
+        price: 39.9,
+        rating: 5
+      }
+    ]);
+  }
+
+
+}
+
+
+/*
+TODO:
+- Datenmodell (Struktur)
+- Daten
+- Komponente für 1 Buch
+*/

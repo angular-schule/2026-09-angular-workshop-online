560e44b4e271faf3d34218054e734a64a58d07bf
Ferdinand Malcher <ferdinand@malcher.media>
2026-09-10 13:44:39 +0200

    Service durch Stub ersetzen

--- book-rating/src/app/books/dashboard-page/dashboard-page.spec.ts
+++ book-rating/src/app/books/dashboard-page/dashboard-page.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { DashboardPage } from './dashboard-page';
+import { BookRatingHelper } from '../shared/book-rating-helper';
+import { Book } from '../shared/book';
 
 describe('DashboardPage', () => {
   let component: DashboardPage;
@@ -8,10 +10,25 @@ describe('DashboardPage', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [DashboardPage],
+      providers: [
+        {
+          provide: BookRatingHelper,
+          useValue: {
+            rateUp: (book: Book) => book,
+            rateDown: (book: Book) => book,
+          }
+        }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(DashboardPage);
+
+    // TS-Klasseninstanz
     component = fixture.componentInstance;
+
+    // DOM-Element
+    // fixture.nativeElement.querySelector('p')
+
     await fixture.whenStable();
   });
 

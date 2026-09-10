2bb73292a3adbeae879f0fdcdffbe0185d88ef2a
Ferdinand Malcher <ferdinand@malcher.media>
2026-09-10 14:31:14 +0200

    Inputs setzen im Test

--- book-rating/src/app/books/book-card/book-card.spec.ts
+++ book-rating/src/app/books/book-card/book-card.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BookCard } from './book-card';
+import { inputBinding } from '@angular/core';
 
 describe('BookCard', () => {
   let component: BookCard;
@@ -10,7 +11,20 @@ describe('BookCard', () => {
       imports: [BookCard],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(BookCard);
+    fixture = TestBed.createComponent(BookCard,
+      {
+        bindings: [
+          inputBinding('book', () => ({
+            isbn: '435435',
+            title: 'Angular',
+            description: '',
+            rating: 5,
+            price: 10,
+            authors: []
+          }))
+        ]
+      }
+    );
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
--- book-rating/src/app/books/rating-display/rating-display.spec.ts
+++ book-rating/src/app/books/rating-display/rating-display.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RatingDisplay } from './rating-display';
+import { inputBinding } from '@angular/core';
 
 describe('RatingDisplay', () => {
   let component: RatingDisplay;
@@ -10,7 +11,11 @@ describe('RatingDisplay', () => {
       imports: [RatingDisplay],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(RatingDisplay);
+    fixture = TestBed.createComponent(RatingDisplay, {
+      bindings: [
+        inputBinding('value', () => 3)
+      ]
+    });
     component = fixture.componentInstance;
     await fixture.whenStable();
   });

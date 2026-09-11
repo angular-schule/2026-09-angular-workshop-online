7e1c07130f0b5e60f72bb4063504ec27ad01545b
Ferdinand Malcher <ferdinand@malcher.media>
2026-09-11 12:08:54 +0200

    BookDetailsPage anlegen

---
+++ book-rating/src/app/books/book-details-page/book-details-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-book-details-page',
+  styleUrl: './book-details-page.scss',
+  templateUrl: './book-details-page.html',
+})
+export class BookDetailsPage {}

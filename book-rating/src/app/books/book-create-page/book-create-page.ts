727d945e51ce25b6467ac4edb4a4afff35b244df
Ferdinand Malcher <ferdinand@malcher.media>
2026-09-14 14:10:20 +0200

    Signal Forms: Grundlagen und CSS-Klasse für Zustand

--- book-rating/src/app/books/book-create-page/book-create-page.ts
+++ book-rating/src/app/books/book-create-page/book-create-page.ts
@@ -1,9 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Book } from '../shared/book';
+import { form, FormField, FormRoot, max, maxLength, min, minLength, pattern, required } from '@angular/forms/signals';
+import { JsonPipe } from '@angular/common';
 
 @Component({
-  imports: [],
+  imports: [FormField, JsonPipe, FormRoot],
   selector: 'app-book-create-page',
   styleUrl: './book-create-page.scss',
   templateUrl: './book-create-page.html',
 })
-export class BookCreatePage {}
+export class BookCreatePage {
+  // Data Model
+  protected readonly bookFormData = signal<Book>({
+    isbn: '',
+    title: '',
+    description: '',
+    rating: 1,
+    price: 0,
+    authors: []
+  });
+
+  // Form Model
+  protected readonly bookForm = form(
+    this.bookFormData,
+    path => {
+      required(path.isbn);
+      pattern(path.isbn, /^\d+$/);
+      minLength(path.isbn, 10);
+      maxLength(path.isbn, 13);
+      
+      required(path.title);
+      
+      required(path.price);
+
+      required(path.rating);
+      min(path.rating, 1);
+      max(path.rating, 5);
+    }
+  );
+}

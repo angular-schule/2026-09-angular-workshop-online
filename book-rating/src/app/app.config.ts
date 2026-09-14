5d40339997ac1d7ab7519132aa0483d7c0b52ca9
Ferdinand Malcher <ferdinand@malcher.media>
2026-09-14 11:44:51 +0200

    Route Preloading

--- book-rating/src/app/app.config.ts
+++ book-rating/src/app/app.config.ts
@@ -1,10 +1,10 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { PreloadAllModules, provideRouter, withComponentInputBinding, withPreloading } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes, withComponentInputBinding())
+    provideRouter(routes, withComponentInputBinding(), withPreloading(PreloadAllModules))
   ]
 };

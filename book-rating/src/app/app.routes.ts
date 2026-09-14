a931183739270ec34188ca69e9b45b9d10193961
Ferdinand Malcher <ferdinand@malcher.media>
2026-09-14 11:04:16 +0200

    Routen-Präfix auslagern mit `children`

--- book-rating/src/app/app.routes.ts
+++ book-rating/src/app/app.routes.ts
@@ -7,6 +7,9 @@ export const routes: Routes = [
     // bei Weiterleitung vom leeren Pfad (fast) immer pathMatch:full nötig
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomePage, title: 'Home' },
-    ...booksRoutes,
+    {
+        path: 'books',
+        children: booksRoutes
+    },
     { path: '**', component: ErrorPage, title: 'Fehler' }
 ];
--- book-rating/src/app/books/books.routes.ts
+++ book-rating/src/app/books/books.routes.ts
@@ -5,10 +5,10 @@ import { BookCreatePage } from "./book-create-page/book-create-page";
 import { BookSearchPage } from "./book-search-page/book-search-page";
 
 export const booksRoutes: Routes = [
-    { path: 'books', component: DashboardPage, title: 'Dashboard' },
-    { path: 'books/create', component: BookCreatePage, title: 'Erstellen' },
-    { path: 'books/search', component: BookSearchPage, title: 'Suche' },
-    { path: 'books/:isbn', component: BookDetailsPage, title: 'Details' },
+    { path: '', component: DashboardPage, title: 'Dashboard' },
+    { path: 'create', component: BookCreatePage, title: 'Erstellen' },
+    { path: 'search', component: BookSearchPage, title: 'Suche' },
+    { path: ':isbn', component: BookDetailsPage, title: 'Details' },
 ];
 
 // Resolver für Title:

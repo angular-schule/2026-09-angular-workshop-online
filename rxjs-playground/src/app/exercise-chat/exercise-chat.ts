445cb38301aa6f667822f57bf070333be9e5e64f
Ferdinand Malcher <ferdinand@malcher.media>
2026-09-15 14:02:37 +0200

    RxJS: Lösungsansätze für Chat und Dragdrop

--- rxjs-playground/src/app/exercise-chat/exercise-chat.ts
+++ rxjs-playground/src/app/exercise-chat/exercise-chat.ts
@@ -33,11 +33,17 @@ export class ExerciseChat {
      * - concat (Emit values from source 1, when complete, subscribe to source 2...)
      * - race (The observable to emit first is used.)
      * - forkJoin (When all observables complete, emit the last emitted value from each.)
+     * - zip
+     * - combineLatest1
      */
 
     /**************!!**************/
 
-     EMPTY.subscribe({                                   
+     merge(
+      this.#msg.julia$,
+      this.#msg.georg$,
+      this.#msg.john$,
+     ).subscribe({                                   
       next: e => this.log(e),
       error: err => this.log('❌ ERROR: ' + err),
       complete: () => this.log('✅ All members left')
--- rxjs-playground/src/app/exercise-dragdrop/exercise-dragdrop.ts
+++ rxjs-playground/src/app/exercise-dragdrop/exercise-dragdrop.ts
@@ -33,6 +33,7 @@ export class ExerciseDragdrop {
 
     /******************************/
 
+    // this.mouseMove$.subscribe(e => this.setTargetPosition(e))
     
     /******************************/
   }

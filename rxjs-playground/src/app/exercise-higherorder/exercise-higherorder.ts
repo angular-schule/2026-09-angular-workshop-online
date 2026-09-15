2b58b79b6d5baed7b5cc9a100afcfdbde7cd0378
Ferdinand Malcher <ferdinand@malcher.media>
2026-09-15 13:56:50 +0200

    RxJS: Higher-Order (Anfang)

--- rxjs-playground/src/app/exercise-higherorder/exercise-higherorder.ts
+++ rxjs-playground/src/app/exercise-higherorder/exercise-higherorder.ts
@@ -30,6 +30,7 @@ export class ExerciseHigherorder {
     /**************!!**************/
 
     this.result$ = this.source$.pipe(
+      // map(tier => this.#es.echo(tier))
     );
 
     /**************!!**************/
